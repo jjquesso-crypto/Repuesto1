2e0b8207dc5c0aa0b623acfa56d5231c83b52bc4
juana jael gomez <juanabruno118@gmail.com>
2026-03-06 23:55:48 -0400

    Agregar archivos de proyecto.

---
+++ Repuesto1/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace Repuesto1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            menuStrip1 = new MenuStrip();
+            archivoToolStripMenuItem = new ToolStripMenuItem();
+            nCFToolStripMenuItem = new ToolStripMenuItem();
+            cajaToolStripMenuItem = new ToolStripMenuItem();
+            ventasToolStripMenuItem = new ToolStripMenuItem();
+            inventarioToolStripMenuItem = new ToolStripMenuItem();
+            cporCToolStripMenuItem = new ToolStripMenuItem();
+            cporCToolStripMenuItem1 = new ToolStripMenuItem();
+            bancoToolStripMenuItem = new ToolStripMenuItem();
+            nominaToolStripMenuItem = new ToolStripMenuItem();
+            configuracionToolStripMenuItem = new ToolStripMenuItem();
+            herramientasToolStripMenuItem = new ToolStripMenuItem();
+            mantenimientoToolStripMenuItem = new ToolStripMenuItem();
+            dGIIToolStripMenuItem = new ToolStripMenuItem();
+            ayudaToolStripMenuItem = new ToolStripMenuItem();
+            efrenisnetToolStripMenuItem = new ToolStripMenuItem();
+            pictureBox1 = new PictureBox();
+            progressBar1 = new ProgressBar();
+            toolStrip1 = new ToolStrip();
+            toolStripButton2 = new ToolStripButton();
+            toolStripButton3 = new ToolStripButton();
+            toolStripButton4 = new ToolStripButton();
+            toolStripButton5 = new ToolStripButton();
+            toolStripButton6 = new ToolStripButton();
+            toolStripButton1 = new ToolStripButton();
+            linkLabel1 = new LinkLabel();
+            linkLabel2 = new LinkLabel();
+            linkLabel3 = new LinkLabel();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            toolStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Font = new Font("Bell MT", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { archivoToolStripMenuItem, nCFToolStripMenuItem, cajaToolStripMenuItem, ventasToolStripMenuItem, inventarioToolStripMenuItem, cporCToolStripMenuItem, cporCToolStripMenuItem1, bancoToolStripMenuItem, nominaToolStripMenuItem, configuracionToolStripMenuItem, herramientasToolStripMenuItem, mantenimientoToolStripMenuItem, dGIIToolStripMenuItem, ayudaToolStripMenuItem, efrenisnetToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Padding = new Padding(0);
+            menuStrip1.Size = new Size(911, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            archivoToolStripMenuItem.Size = new Size(58, 24);
+            archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // nCFToolStripMenuItem
+            // 
+            nCFToolStripMenuItem.Font = new Font("Segoe UI", 8F);
+            nCFToolStripMenuItem.Name = "nCFToolStripMenuItem";
+            nCFToolStripMenuItem.Size = new Size(40, 24);
+            nCFToolStripMenuItem.Text = "NCF";
+            // 
+            // cajaToolStripMenuItem
+            // 
+            cajaToolStripMenuItem.Name = "cajaToolStripMenuItem";
+            cajaToolStripMenuItem.Size = new Size(40, 24);
+            cajaToolStripMenuItem.Text = "Caja";
+            // 
+            // ventasToolStripMenuItem
+            // 
+            ventasToolStripMenuItem.Name = "ventasToolStripMenuItem";
+            ventasToolStripMenuItem.Size = new Size(52, 24);
+            ventasToolStripMenuItem.Text = "Ventas";
+            // 
+            // inventarioToolStripMenuItem
+            // 
+            inventarioToolStripMenuItem.Name = "inventarioToolStripMenuItem";
+            inventarioToolStripMenuItem.Size = new Size(69, 24);
+            inventarioToolStripMenuItem.Text = "Inventario";
+            // 
+            // cporCToolStripMenuItem
+            // 
+            cporCToolStripMenuItem.Name = "cporCToolStripMenuItem";
+            cporCToolStripMenuItem.Size = new Size(52, 24);
+            cporCToolStripMenuItem.Text = "CporC";
+            // 
+            // cporCToolStripMenuItem1
+            // 
+            cporCToolStripMenuItem1.Name = "cporCToolStripMenuItem1";
+            cporCToolStripMenuItem1.Size = new Size(52, 24);
+            cporCToolStripMenuItem1.Text = "CporC";
+            // 
+            // bancoToolStripMenuItem
+            // 
+            bancoToolStripMenuItem.Name = "bancoToolStripMenuItem";
+            bancoToolStripMenuItem.Size = new Size(48, 24);
+            bancoToolStripMenuItem.Text = "Banco";
+            // 
+            // nominaToolStripMenuItem
+            // 
+            nominaToolStripMenuItem.Name = "nominaToolStripMenuItem";
+            nominaToolStripMenuItem.Size = new Size(57, 24);
+            nominaToolStripMenuItem.Text = "Nomina";
+            // 
+            // configuracionToolStripMenuItem
+            // 
+            configuracionToolStripMenuItem.Name = "configuracionToolStripMenuItem";
+            configuracionToolStripMenuItem.Size = new Size(88, 24);
+            configuracionToolStripMenuItem.Text = "Configuracion";
+            // 
+            // herramientasToolStripMenuItem
+            // 
+            herramientasToolStripMenuItem.Name = "herramientasToolStripMenuItem";
+            herramientasToolStripMenuItem.Size = new Size(85, 24);
+            herramientasToolStripMenuItem.Text = "Herramientas";
+            // 
+            // mantenimientoToolStripMenuItem
+            // 
+            mantenimientoToolStripMenuItem.Name = "mantenimientoToolStripMenuItem";
+            mantenimientoToolStripMenuItem.Size = new Size(97, 24);
+            mantenimientoToolStripMenuItem.Text = "Mantenimientos";
+            // 
+            // dGIIToolStripMenuItem
+            // 
+            dGIIToolStripMenuItem.Name = "dGIIToolStripMenuItem";
+            dGIIToolStripMenuItem.Size = new Size(45, 24);
+            dGIIToolStripMenuItem.Text = "DGII";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            ayudaToolStripMenuItem.Size = new Size(50, 24);
+            ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // efrenisnetToolStripMenuItem
+            // 
+            efrenisnetToolStripMenuItem.Name = "efrenisnetToolStripMenuItem";
+            efrenisnetToolStripMenuItem.Size = new Size(69, 24);
+            efrenisnetToolStripMenuItem.Text = "efrenis.net";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 56);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(911, 443);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(317, 27);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(100, 23);
+            progressBar1.TabIndex = 3;
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.BackColor = Color.White;
+            toolStrip1.BackgroundImageLayout = ImageLayout.None;
+            toolStrip1.Font = new Font("Segoe UI", 7F);
+            toolStrip1.GripStyle = ToolStripGripStyle.Hidden;
+            toolStrip1.ImageScalingSize = new Size(30, 30);
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripButton2, toolStripButton3, toolStripButton4, toolStripButton5, toolStripButton6, toolStripButton1 });
+            toolStrip1.Location = new Point(0, 24);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Padding = new Padding(0);
+            toolStrip1.RenderMode = ToolStripRenderMode.Professional;
+            toolStrip1.Size = new Size(911, 49);
+            toolStrip1.TabIndex = 4;
+            toolStrip1.Text = "toolStrip1";
+            toolStrip1.ItemClicked += toolStrip1_ItemClicked;
+            // 
+            // toolStripButton2
+            // 
+            toolStripButton2.Image = (Image)resources.GetObject("toolStripButton2.Image");
+            toolStripButton2.ImageTransparentColor = Color.Magenta;
+            toolStripButton2.Name = "toolStripButton2";
+            toolStripButton2.Size = new Size(45, 46);
+            toolStripButton2.Text = "Facturas";
+            toolStripButton2.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // toolStripButton3
+            // 
+            toolStripButton3.Image = (Image)resources.GetObject("toolStripButton3.Image");
+            toolStripButton3.ImageTransparentColor = Color.Magenta;
+            toolStripButton3.Name = "toolStripButton3";
+            toolStripButton3.Size = new Size(48, 46);
+            toolStripButton3.Text = "Compras";
+            toolStripButton3.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // toolStripButton4
+            // 
+            toolStripButton4.Image = (Image)resources.GetObject("toolStripButton4.Image");
+            toolStripButton4.ImageTransparentColor = Color.Magenta;
+            toolStripButton4.Name = "toolStripButton4";
+            toolStripButton4.Size = new Size(54, 46);
+            toolStripButton4.Text = "Productos";
+            toolStripButton4.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // toolStripButton5
+            // 
+            toolStripButton5.Image = (Image)resources.GetObject("toolStripButton5.Image");
+            toolStripButton5.ImageTransparentColor = Color.Magenta;
+            toolStripButton5.Name = "toolStripButton5";
+            toolStripButton5.Size = new Size(63, 46);
+            toolStripButton5.Text = "Proveedores";
+            toolStripButton5.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // toolStripButton6
+            // 
+            toolStripButton6.Image = (Image)resources.GetObject("toolStripButton6.Image");
+            toolStripButton6.ImageTransparentColor = Color.Magenta;
+            toolStripButton6.Name = "toolStripButton6";
+            toolStripButton6.Size = new Size(46, 46);
+            toolStripButton6.Text = "Ingresos";
+            toolStripButton6.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // toolStripButton1
+            // 
+            toolStripButton1.Alignment = ToolStripItemAlignment.Right;
+            toolStripButton1.Image = (Image)resources.GetObject("toolStripButton1.Image");
+            toolStripButton1.ImageTransparentColor = Color.Magenta;
+            toolStripButton1.Margin = new Padding(0, 1, 0, 1);
+            toolStripButton1.Name = "toolStripButton1";
+            toolStripButton1.Size = new Size(34, 47);
+            toolStripButton1.Text = "Salir";
+            toolStripButton1.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.DisabledLinkColor = Color.Transparent;
+            linkLabel1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            linkLabel1.ForeColor = SystemColors.ActiveCaptionText;
+            linkLabel1.LinkColor = Color.Black;
+            linkLabel1.Location = new Point(747, 226);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(89, 17);
+            linkLabel1.TabIndex = 5;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Iniciar sesion";
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.DisabledLinkColor = Color.Transparent;
+            linkLabel2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            linkLabel2.ForeColor = SystemColors.ActiveCaptionText;
+            linkLabel2.LinkColor = Color.Black;
+            linkLabel2.Location = new Point(747, 269);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(88, 17);
+            linkLabel2.TabIndex = 6;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "Cerrar sesion";
+            // 
+            // linkLabel3
+            // 
+            linkLabel3.AutoSize = true;
+            linkLabel3.DisabledLinkColor = Color.Transparent;
+            linkLabel3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            linkLabel3.ForeColor = SystemColors.ActiveCaptionText;
+            linkLabel3.LinkBehavior = LinkBehavior.AlwaysUnderline;
+            linkLabel3.LinkColor = Color.Black;
+            linkLabel3.Location = new Point(747, 310);
+            linkLabel3.Name = "linkLabel3";
+            linkLabel3.Size = new Size(129, 17);
+            linkLabel3.TabIndex = 7;
+            linkLabel3.TabStop = true;
+            linkLabel3.Text = "Cambiar de Usuario";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Gray;
+            ClientSize = new Size(911, 495);
+            Controls.Add(linkLabel3);
+            Controls.Add(linkLabel1);
+            Controls.Add(linkLabel2);
+            Controls.Add(toolStrip1);
+            Controls.Add(progressBar1);
+            Controls.Add(pictureBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "MENU PRINCIPAL | COMPAÑÍA ACTIVA : SISFACON EXPRESS | EN PC #:1 | USUARIO: MARIA | CONEXIÓN: REMOTA, IP:SERVIDOR";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem archivoToolStripMenuItem;
+        private ToolStripMenuItem nCFToolStripMenuItem;
+        private ToolStripMenuItem cajaToolStripMenuItem;
+        private ToolStripMenuItem ventasToolStripMenuItem;
+        private ToolStripMenuItem inventarioToolStripMenuItem;
+        private ToolStripMenuItem cporCToolStripMenuItem;
+        private ToolStripMenuItem cporCToolStripMenuItem1;
+        private ToolStripMenuItem bancoToolStripMenuItem;
+        private ToolStripMenuItem nominaToolStripMenuItem;
+        private ToolStripMenuItem configuracionToolStripMenuItem;
+        private ToolStripMenuItem herramientasToolStripMenuItem;
+        private ToolStripMenuItem mantenimientoToolStripMenuItem;
+        private ToolStripMenuItem dGIIToolStripMenuItem;
+        private ToolStripMenuItem ayudaToolStripMenuItem;
+        private ToolStripMenuItem efrenisnetToolStripMenuItem;
+        private PictureBox pictureBox1;
+        private ProgressBar progressBar1;
+        private ToolStrip toolStrip1;
+        private ToolStripButton toolStripButton1;
+        private ToolStripButton toolStripButton2;
+        private ToolStripButton toolStripButton3;
+        private ToolStripButton toolStripButton4;
+        private ToolStripButton toolStripButton5;
+        private ToolStripButton toolStripButton6;
+        private LinkLabel linkLabel1;
+        private LinkLabel linkLabel2;
+        private LinkLabel linkLabel3;
+    }
+}
---
+++ Repuesto1/Form1.cs
@@ -0,0 +1,35 @@
+namespace Repuesto1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
